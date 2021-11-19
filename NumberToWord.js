85dfd2e4370a1858b2e77026c7bbc64131d39324
Prajwal <prajwal.zurunge12@gmail.com>
2021-11-19 21:11:43 +0530

    adding number to word program

--- NumberToWord.js
+++ NumberToWord.js
@@ -1,42 +1,26 @@
-//Problem 1 - Read a single digit number and write the number in word
-const pro = require("prompt-sync");
-const prompt = pro();
-
-let number = prompt("Enter a single digit number (0-9):");
-
-switch(number)
-{
-    case '1' : 
-        console.log("One");
-    break;
-    case '2' : 
-        console.log("Two");
-    break;
-    case '3' : 
-        console.log("Three");
-    break;
-    case '4':
-        console.log("Four");
-    break;
-    case '5':
-        console.log("Five");
-    break;
-    case '6':
-        console.log("Six");
-    break;
-    case '7':
-        console.log("Seven");
-    break;
-    case '8':
-        console.log("Eight");
-    break;
-    case '9':
-        console.log("Nine");
-    break;
-    case '0':
-        console.log("Zero");
-    break;
-   default :
-        console.log("Not a singke digit input");
-   break;
+//Problem - 1
+let number = Math.floor(Math.random()*10);
+console.log("Number : "+number);
+if (number == 1) {
+    console.log("One");
+} else if (number == 2) {
+    console.log("Two");
+} else if (number == 3) {
+    console.log("Three");
+} else if (number == 4) {
+    console.log("Four");
+} else if (number == 5) {
+    console.log("Five");
+} else if (number == 6) {
+    console.log("Six");
+} else if (number == 7) {
+    console.log("Seven");
+} else if (number == 8) {
+    console.log("Eight");
+} else if (number == 9) {
+    console.log("Nine");
+} else if (number == 0) {
+    console.log("Zero");
+} else {
+    console.log("Invalid number");
 }
